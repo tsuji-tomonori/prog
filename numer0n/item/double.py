1c22dabf4eb5f6013547576e5e6bd82d35340dcf
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2019-05-24 01:47:13 +0900

    update double

---
+++ numer0n/item/double.py
@@ -0,0 +1,29 @@
+import sys; sys.path.append('../module')
+import standard_numer0n as nm
+
+def use(info,turn,digit=3):
+    while True:
+        idx = input(f"{info[not turn]['name']}:表示させたい桁を指定してください")
+        fin_flag, idx = check_idx(idx)
+        if fin_flag: break
+        else: print(idx)
+    print("{0}さんの[{1}]桁目の数値は[{2}]です".format(
+                info[not turn]["name"],
+                idx,info[turn]["ans"][idx]))
+    
+    value, eb = nm.call(info,turn)
+    print("{} -> {} : {} ({})".format(info[turn]["name"],
+                info[not turn]["name"],eb,value))
+    if nm.isfin(eb,digit):
+        return (True,turn)
+    else: return (False,turn)
+
+def check_idx(idx):
+    if not idx.isdigit():
+        return (False,"整数値ではありません")
+    idx = int(idx) - 1
+    if not (0 <= idx < 3):
+        return (False,"範囲外の数値を入力しています")
+    return (True ,idx)
+
+use(nm.init_game(),False)
